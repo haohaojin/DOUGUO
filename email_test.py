8a9e6706e0976b321bd072278c9b185f77f11f89
haohaojin <hao.jin@outlook.com>
2016-01-07 14:53:03 +0800

    NA

--- email_test.py
+++ email_test.py
@@ -4,7 +4,7 @@ from email.mime.text import MIMEText
 msg = MIMEText("The body of the email is here")
 msg['Subject'] = "An Email Alert"
 msg['From'] = "python@breadmum.com"
-msg['To'] = "hao.jin@outlook.com"
+msg['To'] = "enfice@hotmail.com"
 s = smtplib.SMTP('mail.breadmum.com')
 s.login('python@breadmum.com','python')
 s.send_message(msg)
